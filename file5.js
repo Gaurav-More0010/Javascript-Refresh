d8390b9a0f17ab905a30c4b0acd564cd8bd2dcd3
Gaurav More <gauravmore0010@gmail.com>
2023-10-05 13:39:25 +0530

    "Basic Array"

---
+++ file5.js
@@ -0,0 +1,33 @@
+//Array Method
+// //length
+// var a = [10, 20, 30];
+// console.log(a.length);
+
+// //shift
+// var s = [1, 23, 4, 6];
+// console.log(s.shift());
+// console.log(s);
+
+// var sh = [];
+// console.log(sh);
+
+//unshift
+
+// var unsh = [1, 4, 6, 3, 34, 789, 45];
+// console.log(unsh.unshift("1111"));
+// console.log(unsh);
+
+//pop
+// var p = [12, 4, 5, 7, 9, 2];
+// console.log(p.pop());
+// console.log(p);
+
+//indexof
+
+// var i = [10, 30, 40];
+// console.log(i.indexOf(40));
+// console.log(i);
+
+//lastIndexof
+// var i = [1, 2, 4, 5, 7, 8, 9, 11, 23, 45];
+// console.log(i.lastIndexOf(9));
