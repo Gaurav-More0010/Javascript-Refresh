37e8b57060d91d8b6e91f8b9380ac0b427d00f8f
Gaurav More <gauravmore0010@gmail.com>
2023-10-06 11:33:13 +0530

    "Functions"

---
+++ functions.js
@@ -0,0 +1,17 @@
+// function myname() {
+//   console.log("Funntionnnnnnnnn");
+// }
+// //myname();
+
+// function addNumber(number1, number2) {
+//   console.log(number1 + number2);
+// }
+// const result = addNumber(5, 4);
+
+// function addNumber(number1, number2) {
+//   let result = number1 + number2;
+//   return result;
+// }
+// const result = addNumber(5, 4);
+
+// console.log("Final Result", result);
